2789d4c98a9d9ea306832b978d16ead5ee03d1ec
Brandon Moore <branluciano@Brandons-MacBook-Air.local>
2018-09-04 15:06:39 -0700

    user can see single reviews

--- app.js
+++ app.js
@@ -36,12 +36,19 @@ app.get('/', (req, res) => {
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
 })
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 app.listen(3000, () => {
